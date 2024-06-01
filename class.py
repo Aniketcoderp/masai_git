2e5073663838743cb2a9bd8975b75b67d054cff4
Aniketcoderp <aniketsavita18@gmail.com>
2024-06-01 14:43:41 +0530

    add

--- class.py
+++ class.py
@@ -1,10 +1,20 @@
-def sub(s,index,cur):
-    if index==len(s):
-        if cur:
-            print(''.join(cur))
-        return
-    sub(s,index+1,cur+[s[index]])
-    sub(s,index+1,cur)
+def root(n):
+    if n==0 or n==1:
+        return n
+    l=0
+    r=n
+    ans=0
+    while l<=r:
+        mid=(l+r)//2
+        if mid*mid==n:
+            return mid
+        if mid*mid<n:
+            l=mid+1
+            ans=mid
+        else:
+            r=mid-1
+    return ans
 n=4
-s='abcd'
-sub(s,0,[])
+print(root(n))
+n=8
+print(root(n))
